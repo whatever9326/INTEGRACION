e84eaae8ce76f20eba48897810ac05440bfdaf04
whatever9326 <whatever9326@gmail.com>
2025-06-20 10:32:22 -0600

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+
+export async function exportToExcel(counts) {
+  const XLSX = await import('https://cdn.sheetjs.com/xlsx-0.20.0/package/xlsx.mjs');
+  const headers = Object.keys(counts);
+  const row = headers.map(h => counts[h]);
+  const data = [headers, row];
+
+  const worksheet = XLSX.utils.aoa_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'SKUs');
+
+  XLSX.writeFile(workbook, 'resultado.xlsx');
+}
